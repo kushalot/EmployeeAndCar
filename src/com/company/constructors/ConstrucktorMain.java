0170b640021afd064ed0c87ea5a11ae2414455e0
kushalot <38114239+kushalot@users.noreply.github.com>
2020-12-08 17:55:37 +0300

    first

---
+++ src/com/company/constructors/ConstrucktorMain.java
@@ -0,0 +1,9 @@
+package com.company.constructors;
+
+import com.company.domainmodel.Employee;
+
+public class ConstrucktorMain {
+    public static void main(String[] args) {
+        Employee employee= new Employee();
+    }
+}
---
+++ src/com/company/domainmodel/Employee.java
@@ -0,0 +1,9 @@
+package com.company.domainmodel;
+
+public class Employee {
+    private static int id;
+    private int salary;
+    private String name;
+    private String position;
+
+}
